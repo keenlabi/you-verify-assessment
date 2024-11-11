34628d854b6d3d53c811ec6211dadcd2845898ca
Keenlabi <keenlabinc@gmail.com>
2024-11-11 10:39:11 +0100

    register the inventory model with its schema and exporting it in the mongoose module

--- src/services/inventory/src/infrastructure/databse/database.module.ts
+++ src/services/inventory/src/infrastructure/databse/database.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import InventorySchema, { InventoryDocument } from './schemas/inventory.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([
+      { name: InventoryDocument.name, schema: InventorySchema }, // Register Inventory model with its schema
+    ]),
+  ],
   exports: [MongooseModule],
 })
 export class DatabaseModule {}
