9df3c97af3ccfb4438da545c30afe230fc84a082
Keenlabi <keenlabinc@gmail.com>
2024-11-12 08:54:48 +0100

    import the INVENTORY_SERVICE for use in the order microservice

--- src/services/order/src/order.module.ts
+++ src/services/order/src/order.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule, OrderRepository } from './infrastructure/databse';
 import { OrderController } from './api/http/order.controller';
 import { OrderService } from './services/order.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ClientsModule.register([
+      {
+        name: 'INVENTORY_SERVICE',
+        transport: Transport.REDIS, // or Transport.TCP, depending on your configuration
+        options: {
+          host: 'localhost',
+          port: 6379, // Port of the Redis server or other transport option
+        },
+      },
+    ]),
+  ],
   controllers: [OrderController],
   providers: [OrderService, OrderRepository],
 })
