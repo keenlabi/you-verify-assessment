d018ed6f7020f95005f937a0e03804264127b7ba
Keenlabi <keenlabinc@gmail.com>
2024-11-12 06:37:59 +0100

    expose the order database module including connection config

--- src/services/order/src/infrastructure/databse/database.module.ts
+++ src/services/order/src/infrastructure/databse/database.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import OrderSchema, { OrderDocument } from './schemas/order.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([
+      { name: OrderDocument.name, schema: OrderSchema }, // Register Inventory model with its schema
+    ]),
+  ],
   exports: [MongooseModule],
 })
 export class DatabaseModule {}
---
+++ src/services/order/src/infrastructure/databse/index.ts
@@ -0,0 +1,3 @@
+export * from './database.module';
+export * from './schemas/order.schema';
+export * from './repositories/order.repository';
