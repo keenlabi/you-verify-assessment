10ffd39bcfae655a31393fe977c02e175089d708
Keenlabi <keenlabinc@gmail.com>
2024-11-12 08:43:19 +0100

    setup microservice app config to communicate with redis

--- src/services/inventory/src/main.ts
+++ src/services/inventory/src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './inventory.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-  //   transport: Transport.REDIS,
-  //   options: {
-  //     host: 'localhost',
-  //     port: 6379,
-  //   },
-  // });
-  // await app.listen();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
+  await app.listen();
 
   // Create the HTTP server and listen on port 3000
   const httpApp = await NestFactory.create(AppModule);
