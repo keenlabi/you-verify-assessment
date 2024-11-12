f6c0a35712064e3b86cf3b530129eeba8d2da113
Keenlabi <keenlabinc@gmail.com>
2024-11-12 09:11:41 +0100

    registered the inventory event client to inventory microservice module

--- src/services/inventory/src/inventory.module.ts
+++ src/services/inventory/src/inventory.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { InventoryController } from './api/http/inventory.controller';
 import { InventoryService } from './services/inventory.service';
 import { DatabaseModule, InventoryRepository } from './infrastructure/databse';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ClientsModule.register([
+      {
+        name: 'INVENTORY_EVENT_CLIENT',
+        transport: Transport.REDIS,
+        options: {
+          host: 'localhost',
+          port: 6379,
+        },
+      },
+    ]),
+  ],
   controllers: [InventoryController],
   providers: [InventoryService, InventoryRepository],
 })
