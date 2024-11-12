ede1d2faea1242ef43fb585f7cdd75a0a04a7958
Keenlabi <keenlabinc@gmail.com>
2024-11-12 08:52:45 +0100

    modified the create order service method to trigger the update-stock-quantity event to confirm if the quantity can be further reduced by the order quantity to determine if order can be created successfully

--- src/services/order/src/services/order.service.ts
+++ src/services/order/src/services/order.service.ts
@@ -1,33 +1,71 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy} from '@nestjs/microservices';
 import { OrderEntity } from 'src/domain/Order.entity';
 import { CreateOrderDto } from 'src/dtos/createOrderDto';
 import { OrderRepository } from 'src/infrastructure/databse';
 import { v4 as UUID } from "uuid";
+import { firstValueFrom } from 'rxjs'
+import { CustomError } from 'src/infrastructure/errors/CustomError';
+import { BadRequestError } from 'src/infrastructure/errors';
 
 @Injectable()
 export class OrderService {
   constructor(
     private readonly orderRepository: OrderRepository,
+    @Inject('INVENTORY_SERVICE') private readonly inventoryClient: ClientProxy,
   ) {}
 
   async createOrder(createOrderData: CreateOrderDto): Promise<OrderEntity> {
-    
-    const newOrderEntity = new OrderEntity(
-      UUID(),
-      createOrderData.stockId,
-      createOrderData.quantity,
-      createOrderData.pricePerUnit,
-      createOrderData.quantity * createOrderData.pricePerUnit
-    )
+    return new Promise(async (resolve, reject)=> {
+      // const isStockQuantityEnough = await this.checkStock(createOrderData.stockId);
+      const isStockQuantityEnough = await this.updateStock(createOrderData.stockId, -createOrderData.quantity);
+      if(!isStockQuantityEnough) {
+        const error = new BadRequestError("Stock level is too low for this order");
+        return reject(error)
+      } 
+  
+      const newOrderEntity = new OrderEntity(
+        UUID(),
+        createOrderData.stockId,
+        createOrderData.quantity,
+        createOrderData.pricePerUnit,
+        createOrderData.quantity * createOrderData.pricePerUnit
+      )
+  
+      const newOrder = await this.orderRepository.create(newOrderEntity);
+      return resolve(new OrderEntity(
+        newOrder.id,
+        newOrder.item.stockReferenceId,
+        newOrder.item.quantity,
+        newOrder.item.pricePerUnit,
+        newOrder.item.quantity * newOrder.item.pricePerUnit
+      ));
+    })
+  }
 
-    const newOrder = await this.orderRepository.create(newOrderEntity);
-    return new OrderEntity(
-      newOrder.id,
-      newOrder.item.stockReferenceId,
-      newOrder.item.quantity,
-      newOrder.item.pricePerUnit,
-      newOrder.item.quantity * newOrder.item.pricePerUnit
+  async checkStock(stockId: string): Promise<boolean> {
+    const stockStatus = await firstValueFrom(
+      this.inventoryClient.send<boolean, string>(
+        { cmd: 'check-stock' },
+        stockId,
+      )
     );
+    
+    return stockStatus;
+  }
+
+  async updateStock(stockId: string, quantity: number): Promise<boolean> {
+    const updateStockStatus = await firstValueFrom(
+      this.inventoryClient.send<boolean, { id:string, quantity:number }>(
+        { cmd: 'update-stock-quantity' },
+        {
+          id: stockId,
+          quantity
+        },
+      )
+    );
+    
+    return updateStockStatus;
   }
 
   async retrieveOrderById(id:string): Promise<OrderEntity|null> {
