4a8196de9646234b851d0a514fd7af19bd808be0
Keenlabi <keenlabinc@gmail.com>
2024-11-12 06:31:36 +0100

    added the config to auto generate timestamps for the inventory document

--- src/services/inventory/src/infrastructure/databse/schemas/inventory.schema.ts
+++ src/services/inventory/src/infrastructure/databse/schemas/inventory.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-@Schema({ collection: 'inventory' })
+@Schema({ collection: 'inventory', timestamps: true })
 export class InventoryDocument extends Document {
     @Prop({ required: true, unique: true })
     id:string;
